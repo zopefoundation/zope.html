b287239c53dc82170689f072121d88c351b6429f
Fred Drake <fred@fdrake.net>
2007-10-29 16:28:51 +0000

    set version info for 1.0.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="zope.html",
-    version="1.0.1dev",
+    version="1.0.0",
     packages=find_packages('src'),
     package_dir={'':'src'},
     namespace_packages=['zope'],
