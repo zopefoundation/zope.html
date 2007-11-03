927a22edeb1982647173b3eed1acee4a1433583c
Stephan Richter <stephan.richter@gmail.com>
2007-11-03 05:20:36 +0000

    vb

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
 setup(name='zope.html',
-      version = '1.0.1',
+      version = '1.1.0',
       author='Zope Corporation and Contributors',
       author_email='zope3-dev@zope.org',
       description='HTML and XHTML Editing Support',
