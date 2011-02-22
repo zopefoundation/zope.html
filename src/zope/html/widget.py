ea982c183563926828c7342a26405a882f3375f1
Alex Chapman <achapman@zope.com>
2011-02-22 22:40:14 +0000

    - upgrade to use CKeditor 3.2.5

--- src/zope/html/widget.py
+++ src/zope/html/widget.py
@@ -63,7 +63,7 @@ var oFCKeditor_%(shortname)s = new FCKeditor(
 class CkeditorWidget(zope.app.form.browser.TextAreaWidget):
 
     editorHeight = 400
-    fckVersion = '3.2.1'
+    fckVersion = '3.2.5'
 
     configurationPath = "/@@/zope_ckconfig.js"
 
