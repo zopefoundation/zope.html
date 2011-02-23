2c8c684b5ed5ce29a1131e07e7cb18ef9dc2bac6
Alex Chapman <achapman@zope.com>
2011-02-23 14:53:33 +0000

    - correct copyright, bump dev ver

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
 setup(name='zope.html',
-      version = '2.3.0dev',
+      version = '2.3.1dev',
       author='Zope Foundation and Contributors',
       author_email='zope-dev@zope.org',
       description='HTML and XHTML Editing Support',
