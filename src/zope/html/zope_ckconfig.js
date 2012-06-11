da7bbfb7a9f3e7ea945c4a71703f43ccbafc672b
Tres Seaver <tseaver@palladion.com>
2012-06-11 18:42:31 +0000

    Repository policy.

--- src/zope/html/zope_ckconfig.js
+++ src/zope/html/zope_ckconfig.js
@@ -1,5 +1,5 @@
 ﻿/*
-Copyright (c) 2003-2009, CKSource - Frederico Knabben. All rights reserved.
+Copyright (c) 2003-2009, Zope Foundation and Contributors.
 For licensing, see LICENSE.html or http://ckeditor.com/license
 */
 
